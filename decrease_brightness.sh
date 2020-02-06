51e233bc5edec4486094a2e4a4cb6465455d1799
Tharwat Abou-Helal <tharwat96@gmail.com>
2020-02-06 11:42:17 +0200

    add backup and restore commands and sddm config

--- decrease_brightness.sh
+++ decrease_brightness.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+
+# requires xbacklight
 read currBrightness < /sys/class/backlight/intel_backlight/brightness
 read maxBrightness < /sys/class/backlight/intel_backlight/max_brightness
-if [ "$currBrightness" -gt 10 ] && [ "$currBrightness" -le 40 ] # 1-40
+if [ "$currBrightness" -gt 1 ] && [ "$currBrightness" -le 15 ] # 1-15
+then
+    xbacklight -dec 0.01
+elif [ "$currBrightness" -ge 15 ] && [ "$currBrightness" -le 40 ] # 15-40
 then
     xbacklight -dec 0.2
-elif [ "$currBrightness" -le 75 ] && [ ! "$currBrightness" -le 40 ] # +40
+elif [ "$currBrightness" -ge 40 ] && [ "$currBrightness" -le 75 ] # 40 - 75
 then
     xbacklight -dec 0.5
-elif [ "$currBrightness" -gt 75 ] && [ "$currBrightness" -le 375 ] # 75-375
+elif [ "$currBrightness" -ge 75 ] && [ "$currBrightness" -le 375 ] # 75-375
 then
     xbacklight -dec 1
-elif [ "$currBrightness" -gt 375 ] # +375
+elif [ "$currBrightness" -gt 375 ] && [ "$currBrightness" -le 800 ] # 375 - 800 
+then
+	xbacklight -dec 3
+elif [ "$currBrightness" -gt 375 ] # +800 
 then
 	xbacklight -dec 5
 fi	
